a71fcffb039e7a91c00d5d8854a5ab357eb998fa
Manish_7 <manish@unitx.tech>
2022-07-08 11:25:25 +0530

    added a function to fetch youtube videos from google youtube Api

--- FetchApi/api/utils.py
+++ FetchApi/api/utils.py
@@ -1,28 +1,42 @@
-#file for helper functions used to fetch and store  youtube videos \
-from api.models import YTVideo
-import argparse
+import os
+from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from FetchApi.settings import (
+    YOUTUBE_API_SERVICE_NAME,
+    YOUTUBE_API_VERSION,
+)
 
 
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = 'AIzaSyCSdKQIjOXAFLkY3mHlGeJVwM0RSs00ymg'
-YOUTUBE_API_SERVICE_NAME = 'youtube'
-YOUTUBE_API_VERSION = 'v3'
+def youtube_fetch(query, check_int, max_results):
+    
+    DEVELOPER_KEY = "AIzaSyCSdKQIjOXAFLkY3mHlGeJVwM0RSs00ymg"
+    try:
+        youtube = build(
+            YOUTUBE_API_SERVICE_NAME,
+            YOUTUBE_API_VERSION,
+            developerKey=DEVELOPER_KEY,
+        )
 
-def youtube_search(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
+        target_ts = (
+            datetime.now() - timedelta(minutes=check_int)
+        ).isoformat() + "Z"
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=options.q,
-    part='id,snippet',
-    maxResults=options.max_results
-  ).execute()
-
-
+        search_response = (
+            youtube.search()
+            .list(
+                q=query,
+                part="snippet",
+                maxResults=max_results,
+                order="date",
+                publishedAfter=target_ts,
+            )
+            .execute()
+        )
+        print(f"Request Succeeded with API_KEY")
+        return search_response
+    except HttpError as e:
+        print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
+        if e.resp.status == 403:
+            print(f"Request Failed with API_KEY")
+        return dict()
