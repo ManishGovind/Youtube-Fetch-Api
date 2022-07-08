acfe4d42292ebc3e5a1ef1e0766393b65d997f50
Manish_7 <manish@unitx.tech>
2022-07-08 13:30:11 +0530

    celery beat schedule added

--- FetchApi/FetchApi/celery.py
+++ FetchApi/FetchApi/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 
@@ -11,6 +12,19 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Looks up for task modules in Django applications and loads them
 app.autodiscover_tasks()
+
+
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 7:30 a.m.
+    'fetchs videos every 30 seconds': {
+        'task': 'tasks.save_videos',
+        'schedule': crontab(seconds=30),
+        'args': (16, 16),
+    },
+}
+
+app.conf.timezone = 'Asia/Kolkata'
 
 @app.task(bind=True)
 def debug_task(self):
